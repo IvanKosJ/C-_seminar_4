637a94fca9d2fc6aede721c8a694cc7bf8a3cc0a
Ivan Kosulin <ivankosj@yandex.ru>
2023-07-28 23:16:41 +0300

    Добавлены 25, 27, 29

---
+++ task25/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    return inputNumber;
+}
+
+int number_A = GetInfo("Введите число A: ");
+int number_B = GetInfo("Введите число B: ");
+Console.WriteLine($"Результат возведения числа {number_A} в {number_B} степень - {ToDegree(number_A, number_B)}");
---
+++ task27/Program.cs
@@ -0,0 +1,32 @@
+﻿void DigitSum(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
+
+int Length(int a)
+{
+    int i = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        i++;
+    }
+    return i;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    return inputNumber;
+}
+
+int number = GetInfo("Введите число: ");
+int len = Length(number);
+DigitSum(number, len);
